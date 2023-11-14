bae3d4d4d5e558572938bcf43ddee26c8ac2c469
Maxim Eremeev <bergent39@gmail.com>
2023-11-15 00:46:38 +0300

    fix: Fixed regular expression to correctly match phone numbers

--- src/server.cpp
+++ src/server.cpp
@@ -20,16 +20,18 @@ void Server::Run() {
 
     // GET Request handler for phone numbers
     // Will return values matching the regexp
-    // Example: +79005553535 -> 9005553535
-    server.Get(R"(/\+?[7-8](\d{10}))", [&](const httplib::Request& req, httplib::Response& res) {
-        auto numbers = req.matches[1];
+    server.Get(R"((/)(\+?[7-8](\d{10})))", [&](const httplib::Request& req, httplib::Response& res) {
+        auto numbers = req.matches[2];
         res.set_content(numbers, "text/plain");
     }); 
 
+    // Check if server is valid
     if (!server.is_valid()) {
        std::cerr << "ERROR: Server is not valid! Check the configuration file\n";
        return;
     }
     std::cout << "Server is running properly\n";
+
+    // Make server listen to the designated host and port
     server.listen(_host, _port);
 }
